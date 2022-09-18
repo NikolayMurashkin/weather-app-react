ccea714a668768e3c977dd98997dc85c66399eb2
Nikolay <m.nikolay1990@gmail.com>
2022-09-18 15:56:06 +0700

    create a current weather widget

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '15692de603mshe0705b9ca70af7bp1dbe49jsnb2a4891a5c0b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "e255fe6afb038680439f30411954e9b4";
