788cdef2136985fea5ac60a48633345dfeacaa36
Nikolay <m.nikolay1990@gmail.com>
2022-09-18 15:57:28 +0700

    Updates

---
+++ precache-manifest.72773f8a1c3806edc5f3f098873bac94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9db2078d2df83c1e4999",
+    "url": "/weather-app-react/static/css/main.1b285bdb.chunk.css"
+  },
+  {
+    "revision": "9db2078d2df83c1e4999",
+    "url": "/weather-app-react/static/js/main.9db2078d.chunk.js"
+  },
+  {
+    "revision": "97bc1625e33b04eb7828",
+    "url": "/weather-app-react/static/js/1.97bc1625.chunk.js"
+  },
+  {
+    "revision": "f87523b966001dcf883e",
+    "url": "/weather-app-react/static/js/2.f87523b9.chunk.js"
+  },
+  {
+    "revision": "80af6af73830056af3e7",
+    "url": "/weather-app-react/static/js/runtime~main.80af6af7.js"
+  },
+  {
+    "revision": "b8545286f3a11372c59c81ff06a22694",
+    "url": "/weather-app-react/index.html"
+  }
+];
